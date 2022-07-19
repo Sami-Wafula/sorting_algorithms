8c25c40a9768fe0781abc51619f87e226aba1474
Sami <samiwafula3257@gmail.com>
2022-07-19 09:13:41 +0300

    updated prototypes for insertion sort

--- sort.h
+++ sort.h
@@ -41,4 +41,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_items(int *array, size_t l, size_t r);
 void bubble_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *l, listint_t *r)
+void insertion_sort_list(listint_t **list)
 #endif
