2d5edc591a8f692ebc5af8e05bf561eb1f6ebbcf
Sami <samiwafula3257@gmail.com>
2022-07-19 09:45:51 +0300

    added all prototypes

--- sort.h
+++ sort.h
@@ -1,46 +1,46 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
-#include <stdlib.h>
 
-#ifndef TRUE
-#define TRUE 1
-#endif
-#ifndef FALSE
-#define FALSE 0
-#endif
-#ifndef UNDEF
-#define UNDEF -1
-#endif
-#ifndef SWAP
-#define SWAP(array, a, b) \
-	{                       \
-		temp = array[a];       \
-		array[a] = array[b];  \
-		array[b] = temp;       \
-	}
-#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * struct listint_s - Represents a doubly linked list node.
- * @n: Integer stored in the node.
- * @prev: Pointer to the previous element of the list.
- * @next: Pointer to the next element of the list.
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-	/* Integer stored in the node. */
 	const int n;
-	/* Pointer to the previous element of the list. */
 	struct listint_s *prev;
-	/* Pointer to the next element of the list. */
 	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap_items(int *array, size_t l, size_t r);
+
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
-void swap_nodes(listint_t **list, listint_t *l, listint_t *r)
-void insertion_sort_list(listint_t **list)
-#endif
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* QuickSort helper functions */
+void quicker_sort(int *array, size_t size, int lo, int hi);
+int partition(int *array, size_t size, int lo, int hi);
+
+void swap(int *ptr_a, int *ptr_b);
+void swap_ints(int *a, int *b);
+
+#endif /* SORT_H */
